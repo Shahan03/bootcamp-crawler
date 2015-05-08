d8680438fea3ec09100069fbd897dba299c185ba
Shahan03 <shahan03@ymail.com>
2015-05-08 16:51:00 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,28 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+import sys
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+driver = webdriver.Firefox()
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# uncomment if using Phantomjs
+#driver = webdriver.PhantomJS()
+
+url = 'http://regsys.fbr.gov.pk/Registration/onlinesearchTaxpayer.aspx?rand=2'
+driver.get(url)
+cardNumber = '3540437900355'
+try:
+	dropDown = driver.find_element_by_id('ctl00_ContentPlaceHolder1_ctrlOnlineSearch_ddlSearchType')
+	for option in dropDown.find_elements_by_tag_name('option'):
+		if option.text == "CNIC":
+			option.click()
+	sleep(3)
+	searchButton = driver.find_element_by_id('ctl00_ContentPlaceHolder1_ctrlOnlineSearch_btnSearch')
+	textField = driver.find_element_by_id('ctl00_ContentPlaceHolder1_ctrlOnlineSearch_txtSearch2')
+	#textField = driver.find_element_by_id('PralAJAXControls_ctl00_ContentPlaceHolder1_ctrlOnlineSearch_txtSearch2__')
+	#textField = driver.find_element_by_name('ctl00$ContentPlaceHolder1$ctrlOnlineSearch$txtSearch2')
+	textField.send_keys(cardNumber)
+	searchButton.click()
+except:
+	print "Error: "+str(sys.exc_info())+" at line: "+str(sys.exc_traceback.tb_lineno)
+	exit(-1)
